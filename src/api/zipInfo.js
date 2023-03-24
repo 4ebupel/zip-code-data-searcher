654602066dbd8849c9defc56fcfa8df8e1795fd5
Artem Rymarchuk <rumarchyk204@gmail.com>
2023-03-24 14:42:27 +0100

    Lord, have mercy

--- src/api/zipInfo.js
+++ src/api/zipInfo.js
@@ -1,19 +1,16 @@
-import { client } from "../httpClient"
-// import axios from "axios";
+import axios from "axios";
 
 export const getInfo = (zip) => {
-  // const params = {
-  //   access_key: '6d61bdcbeed61f15a150ba4d1d0d5956',
-  //   query: zip
-  // }
-  // return client.get(`/current`, { params })
-  //   .then(response => {
-  //     const apiResponse = response.data;
-  //     return apiResponse;
-  //   }).catch(error => {
-  //     console.log(error);
-  //   });
 
-  return fetch(`http://api.weatherstack.com/current?access_key=6d61bdcbeed61f15a150ba4d1d0d5956&query=${zip}`)
-  .then(response => response.json);
+  const options = {
+    method: 'GET',
+    url: `https://api.tomorrow.io/v4/weather/forecast?location=${zip}&timesteps=1d&units=metric&apikey=TUzTweSdassA6U6IR6Yb54i4pxXVtFmf`,
+    headers: {accept: 'application/json'}
+  };
+  
+  return axios
+    .request(options)
+    .catch(function (error) {
+      console.error(error);
+    });
 };
--- src/pages/InfoPage.vue
+++ src/pages/InfoPage.vue
@@ -9,45 +9,45 @@ export default {
   data() {
     return {
       info: {
-        "request": {
-          "type": "Zipcode",
-          "query": "36525",
-          "language": "en",
-          "unit": "m"
-        },
-        "location": {
-          "name": "Creola",
-          "country": "USA",
-          "region": "Alabama",
-          "lat": "30.889",
-          "lon": "-88.026",
-          "timezone_id": "America/Chicago",
-          "localtime": "2023-03-23 16:28",
-          "localtime_epoch": 1679588880,
-          "utc_offset": "-5.0"
-        },
-        "current": {
-          "observation_time": "09:28 PM",
-          "temperature": 26,
-          "weather_code": 116,
-          "weather_icons": [
-            "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0002_sunny_intervals.png"
-          ],
-          "weather_descriptions": [
-            "Partly cloudy"
-          ],
-          "wind_speed": 22,
-          "wind_degree": 170,
-          "wind_dir": "S",
-          "pressure": 1021,
-          "precip": 0,
-          "humidity": 58,
-          "cloudcover": 75,
-          "feelslike": 29,
-          "uv_index": 5,
-          "visibility": 16,
-          "is_day": "yes"
-        }
+        // "request": {
+        //   "type": "Zipcode",
+        //   "query": "36525",
+        //   "language": "en",
+        //   "unit": "m"
+        // },
+        // "location": {
+        //   "name": "Creola",
+        //   "country": "USA",
+        //   "region": "Alabama",
+        //   "lat": "30.889",
+        //   "lon": "-88.026",
+        //   "timezone_id": "America/Chicago",
+        //   "localtime": "2023-03-23 16:28",
+        //   "localtime_epoch": 1679588880,
+        //   "utc_offset": "-5.0"
+        // },
+        // "current": {
+        //   "observation_time": "09:28 PM",
+        //   "temperature": 26,
+        //   "weather_code": 116,
+        //   "weather_icons": [
+        //     "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0002_sunny_intervals.png"
+        //   ],
+        //   "weather_descriptions": [
+        //     "Partly cloudy"
+        //   ],
+        //   "wind_speed": 22,
+        //   "wind_degree": 170,
+        //   "wind_dir": "S",
+        //   "pressure": 1021,
+        //   "precip": 0,
+        //   "humidity": 58,
+        //   "cloudcover": 75,
+        //   "feelslike": 29,
+        //   "uv_index": 5,
+        //   "visibility": 16,
+        //   "is_day": "yes"
+        // }
       },
       errorStatus: false,
       isLoading: false,
@@ -57,41 +57,33 @@ export default {
   props: {
     zip: String,
   },
-  async mounted() {
-    try {
-      this.isLoading = true;
-      const smth = await getInfo(this.zip).then(response => JSON.parse(response));
-      this.info = smth;
-      // console.log(this.info);
-    } catch (error) {
-      this.errorStatus = true;
-    } finally {
-      this.isLoading = false;
-    }
-  },
-  // updated() {
-  //   console.log(this.info);
-  // }
+  mounted() {
+    getInfo(this.zip)
+      .then(({ data }) => {
+        this.info = data;
+      });
+  }
 }
 </script>
 
 <template>
   <Header :innerText="`Your data & weather report`" />
   <section v-if="!isLoading" class="background">
+    <h1 v-if="isLoading">Wait a sec...</h1>
     <div class="card" style="width: 18rem;" v-if="!errorStatus">
       <div class="card-body">
-        <h5 class="card-title">Your City is: {{ info.location.name }}</h5>
+        <h5 class="card-title">Your City is: {{ info.location.name.split(',')[0] }}</h5>
         <h6 class="card-subtitle mb-2 text-muted">More data</h6>
         <ul class="list-group list-group-flush">
-          <li class="list-group-item">Your contry is: {{ info.location.country }}</li>
-          <li class="list-group-item">Your state is: {{ info.location.region }}</li>
-          <li class="list-group-item">Your UTC-offset is: {{ info.location.utc_offset }}</li>
+          <li class="list-group-item">Your contry is: {{ info.location.country.split(',')[4] }}</li>
+          <li class="list-group-item">Your state is: {{ info.location.region.split(',')[2] }}</li>
+          <li class="list-group-item">Your ZIP code is: {{ info.location.country.split(',')[3] }}</li>
         </ul>
         <a href="/" class="btn btn-dark">Home</a>
         <a href="#" class="btn btn-dark">Toggle weather</a>
       </div>
       <div class="card-footer">
-        <small class="text-muted">Observation time: {{ info.current.observation_time }}</small>
+        <small class="text-muted">Observation time: {{ info.timelines.daily[0].time }}</small>
       </div>
     </div>
     <div class="card text-white bg-danger mb-3" style="max-width: 18rem;" v-if="errorStatus">
